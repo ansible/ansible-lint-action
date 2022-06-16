fb392c327936de43d1c70e231f818f8ca99a754f
Yong Yan <yanyong-gh@outlook.com>
2022-06-16 12:01:23 -0700

    fix variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ sarif_output_file="${3}"
 
 # if the argument sarif_output_file is set
 # redirect the SARIF output into the specified file name
-if [[ -z $output_file ]]; then
+if [[ -z $sarif_output_file ]]; then
   ansible-lint $verbosity_level $lintable_path;
 else
-  ansible-lint $verbosity_level $lintable_path -f sarif > ${output_file};
+  ansible-lint $verbosity_level $lintable_path -f sarif > ${sarif_output_file};
 fi
